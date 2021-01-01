22a110f453ca9e3ba8561109c9f72ef04279681d
Michelle Chen <mchen0062@gmail.com>
2020-12-31 17:17:38 -0800

    Comment

---
+++ my-app/src/components/FoodTruck.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// FoodTruck component includes: 
+// Menu icon, back button 
+// Card: 
+// Title: Food Truck #, stars, Food Genre, Food Attributes, Food truck picture 
+// Body: description 
+// Buttons: See Menu, Leave Review, Set Favorite - action w/ pop up, Get Directions 
+// Footer Banner 
